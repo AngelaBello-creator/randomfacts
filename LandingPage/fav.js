d6e160f20a0251009eb8c143d609a08f7d51094a
Abaraira <cibersteficodes@gmail.com>
2025-07-23 12:48:25 +0200

    fav.js creado

--- LandingPage/fav.js
+++ LandingPage/fav.js
@@ -1,8 +1,25 @@
-import { getData } from './api.js';
-
 const favorites = [];
-let currentFact = "";
 
-const factContainer = document.getElementById("fact");
-const favoritesContainer = document.getElementById("favorites");
+export function addFavorite (fact) {
+    if (!favorites.includes(fact)) {
+        favorites.push(fact);
+        renderFavorites();
+    }
+}
 
+export function renderFavorites () {
+    const container = document.getElementById("favfact");
+    if (!container) return;
+
+    if (favorites.length === 0) {
+        container.textContent = "You haven't added any favorite facts yet";
+    }
+    else {
+        container.innerHTML= "";
+        favorites.forEach(fact => {
+            const iteration = document.createElement("iteration");
+            iteration.textContent = `${fact}`;
+            container.appendChild(iteration); 
+        });
+    }
+}
